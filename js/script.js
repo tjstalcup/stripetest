1a945099328ad2700c927bff88da26447429c154
Thomas Stalcup Jr <tstalcupjr@gmail.com>
2016-03-31 21:22:20 -0400

    Moves JS to external JS file

--- js/script.js
+++ js/script.js
@@ -9,4 +9,34 @@ $(document).ready(function(){
 			$('#totalPrice').val(total+'00');
 		}
 	});
+
+	var handler = StripeCheckout.configure({
+	  key: 'pk_test_6pRNASCoBOKtIshFeQd4XMUh',
+	  image: 'img/burrito.png',
+	  locale: 'auto',
+	  token: function(token) {
+	    // Use the token to create the charge with a server-side script.
+	    // You can access the token ID with `token.id`
+	    var $form = $('#payment-form');
+	    $form.append($('<input type="hidden" name="stripeToken" />').val(token.id));
+	    $form.append($('<input type="hidden" name="stripeEmail" />').val(token.email));
+	    $form.submit();
+	  }
+	});
+
+	$('#customButton').on('click', function(e) {
+	  // Open Checkout with further options
+	  var total = Math.round($('#totalPrice').val());
+	  handler.open({
+	    name: 'Burritos4Basis',
+	    description: 'Burritos',
+	    amount: total
+	  });
+	  e.preventDefault();
+	});
+
+	// Close Checkout on page navigation
+	$(window).on('popstate', function() {
+	  handler.close();
+	});
 });
